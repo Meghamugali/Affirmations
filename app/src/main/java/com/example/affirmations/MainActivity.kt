0e23c409ab3039037aaab7ec5b9b1a2d58dea034
Megha <meghabmugali@gmail.com>
2022-09-07 16:15:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.affirmations.data.Datasource
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
